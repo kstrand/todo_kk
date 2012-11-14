38aae0e6e60bd9274fc7b9f983b50638e6058b8b
Apprentice <apprentice@devbootcamp.com>
2012-11-13 18:20:56 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KkTodo::Application.config.secret_token = 'd06dcf622de702fce385e2c5e77c7b088cfc7919f1f2983e7132bd6770c17853be71a93657c5fc356913f8b78109d0d91ed390031315041fe1f66db56fe919c1'
