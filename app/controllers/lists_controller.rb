d867e7fbadc1d7696565e3a967e3f59af20966d4
Apprentice <apprentice@devbootcamp.com>
2012-11-13 18:45:17 -0800

    build List and Task resources

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+
+
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    @list.save
+    redirect_to lists_path
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 KkTodo::Application.routes.draw do
+
+
+  resources :lists
+
+  root to: 'lists#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
